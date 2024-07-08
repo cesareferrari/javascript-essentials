56ae2768c5ddc9b02c05e727167e378fe545a517
Cesare Ferrari <cesare@hey.com>
2024-07-08 09:06:53 -0400

    Update promises page

--- javascripts/promises.js
+++ javascripts/promises.js
@@ -1,36 +1,15 @@
 let sayHello = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    resolve("Hi, universe");
-  }, 5000);
-})
-
-sayHello.then(function (msg) {
-  console.log(msg);
-})
-
-
-let goodbye = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("Goodbye!");
-  }, 10000);
-})
+    resolve("Hello world");
+  }, 5000)
+});
 
-goodbye.then((msg) => {
-  console.log(msg);
-})
-
-// chaining
-
-let count = new Promise((resolve, reject) => {
-  resolve(1);
-})
-
-count.then((num) => {
-  console.log(num);
-  return num + 1;
-}).then((num) => {
-  console.log(num);
-  return num + 1;
-}).then((num) => {
-  console.log(num);
-})
+sayHello.
+  then((msg) => {
+    console.log(msg)
+    return msg
+  }).
+  then((msg) => {
+    msg += " and hello universe"
+    console.log(msg);
+  })
