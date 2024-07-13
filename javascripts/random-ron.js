acfa62ad9de9544d716d7d98ef36975cfe43ae81
Cesare Ferrari <cesare@maryandferrari.com>
2024-07-13 07:21:17 -0400

    More async await

--- javascripts/random-ron.js
+++ javascripts/random-ron.js
@@ -6,33 +6,65 @@ let quotes = [];
 
 getQuote();
 
-function getQuote () {
-  fetch(url).
-    then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
+// function getQuote () {
+//   fetch(url).
+//     then((response) => {
+//       if (response.ok) {
+//         return response.json();
+//       }
 
-      throw response.status;
-    }).
-    then((data) => {
-      if (quotes.length > 49) {
-        quotes.shift();
-      }
+//       throw response.status;
+//     }).
+//     then((data) => {
+//       if (quotes.length > 49) {
+//         quotes.shift();
+//       }
 
-      if (quotes.includes(data[0])) {
-        getQuote();
-        return;
-      }
+//       if (quotes.includes(data[0])) {
+//         getQuote();
+//         return;
+//       }
 
-      quoteContainer.textContent = data[0];
-      quotes.push(data[0])
-    }).
-    catch((err) => {
-      quote.textContent = `Something went wrong! ${err}`;
-    })
+//       quoteContainer.textContent = data[0];
+//       quotes.push(data[0])
+//     }).
+//     catch((err) => {
+//       quote.textContent = `Something went wrong! ${err}`;
+//     })
+// }
+
+
+// button.addEventListener("click", getQuote);
+
+
+
+
+async function getQuote () {
+
+  try {
+    let response = await fetch(url);
+    if (!response.ok) throw response.status;
+
+    let data = await response.json();
+    if (!data) throw "No data";
+
+    if (quotes.length > 49) {
+      quotes.shift();
+    }
+
+    if (quotes.includes(data[0])) {
+      getQuote();
+      return;
+    }
+
+    quoteContainer.textContent = data[0];
+    quotes.push(data[0]);
+
+  } catch (error) {
+    quoteContainer.textContent = `Something went wrong! ${error}`;
+  }
 }
 
+getQuote();
 
 button.addEventListener("click", getQuote);
-
