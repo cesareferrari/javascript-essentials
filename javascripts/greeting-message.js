de1bb6d763d5817e6dc8f105b1e38dd8943f7cab
Cesare Ferrari <cesare@maryandferrari.com>
2024-09-14 09:18:41 -0400

    Add event listener to web component

--- javascripts/greeting-message.js
+++ javascripts/greeting-message.js
@@ -16,7 +16,26 @@ class GreetingMessage extends HTMLElement {
   }
 
   connectedCallback () {
-    console.log("Connected.", this);
+    let btn = this.querySelector('button');
+    if (!btn) return;
+    btn.addEventListener("click", function (event) {
+      console.log("Inside event listener `this` is: ", this);
+
+      // get the host component
+      let host = event.target.closest('greeting-message')
+
+      // get the message element
+      let target = host.querySelector('.message')
+      if (!target) return;
+
+      // inject the message into the UI
+      target.textContent = "Hi there, friend! Have a good day."
+
+      // clear the message after 5 seconds
+      setTimeout(function () {
+        target.textContent = "";
+      }, 5000);
+    })
   }
 
   disconnectedCallback () {
